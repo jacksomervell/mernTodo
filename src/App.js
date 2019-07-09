790d1de9986d73ef649003d9b014ac484beba0e7
Jack Somervell <jack.somervell@curtisbanks.co.uk>
2019-07-09 11:51:55 +0100

    react bootstrap switch and font

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CreateTodo from "./components/create-todo.component";
-import EditTodo from "./components/edit-todo.component";
 import WhatIf from "./components/whatif.component";
 import PercTable from "./components/percentages.component";
 import ChipChecker from "./components/chip-checker.component";
 import YouRival from "./components/youvsrival.component";
 import Home from "./components/home.component";
+import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Container>
+          <Navbar expand="lg" bg="primary" variant="dark">
             <Link to="/" className="navbar-brand"><h1>FPL Tools</h1></Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Home</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/whatIf" className="nav-link">What-if Machine</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/percentages" className="nav-link">Player Percentages</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/chip-checker" className="nav-link">Chip Checker</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/you-vs-rival" className="nav-link">You vs Rival</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+            <Navbar.Collapse>
+              <Nav className="mr-auto">
+                <NavDropdown title="Tools" id="basic-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="/whatIf">What-If Machine</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/percentages">Player Ownership Percentages</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/chip-checker">Chip Checker</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/you-vs-rival">You vs a Rival</NavDropdown.Item>
+                </NavDropdown>
+                  <Nav.Link as={Link} to="/blog" >Blog</Nav.Link>
+                  <Nav.Link as={Link} to="/podcasts" >Podcasts</Nav.Link>
+
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
           <br/>
           <Route path="/" exact component={Home} />
           <Route path="/whatIf" component={WhatIf} />
           <Route path="/percentages" component={PercTable} />
           <Route path="/chip-checker" component={ChipChecker} />
           <Route path="/you-vs-rival" component={YouRival} />
-        </div>
+        </Container>
       </Router>
     );
   }
